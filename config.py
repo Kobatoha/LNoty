0348f22b6704956e730831fdaf8265e5334e36c1
Alexandra <kobatoha@gmail.com>
2023-08-01 00:06:30 +0300

    update

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-TOKEN = 'your_token'
+TOKEN = '6190577316:AAHI5i5n_Zf3KMMTpylNyQL37R5WfZ-EAUw'
 
 
 # PostgreSQL db information
 host = 'localhost'
 user = 'postgres'
-password = 'postgres'
-db_name = 'postgres'
+password = 'sirok123'
+db_name = 'lineage2'
 port = 5433
 
 DB_URL = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
