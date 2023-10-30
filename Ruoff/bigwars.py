0702842c9edc95f73c9bf71b2936c60588045aaa
Alexandra <kobatoha@gmail.com>
2023-10-30 13:16:10 +0300

    update bigwar settings

---
+++ Ruoff/bigwars.py
@@ -0,0 +1,5 @@
+# [BIGWAR]
+from Commands.bigwar import bigwar_menu
+# [TOI]
+from Ruoff.BigWars.bigwar_toi import about_bigwar_toi, set_bigwar_toi, remove_bigwar_toi, \
+    bigwar_toi_notification_wrapper
