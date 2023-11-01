00785211dacfa999083a75a9472b52a4067951a7
Alexandra <kobatoha@gmail.com>
2023-11-01 23:58:22 +0300

    update bigwar gardens imports

--- Ruoff/bigwars.py
+++ Ruoff/bigwars.py
@@ -3,3 +3,6 @@ from Commands.bigwar import bigwar_menu
 # [TOI]
 from Ruoff.BigWars.bigwar_toi import about_bigwar_toi, set_bigwar_toi, remove_bigwar_toi, \
     bigwar_toi_notification_wrapper
+# [GARDENS]
+from Ruoff.BigWars.bigwar_gardens import about_bigwar_gardens, set_bigwar_gardens, remove_bigwar_gardens, \
+    bigwar_gardens_notification_wrapper
