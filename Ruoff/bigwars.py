8456fe1f5befd4eadac8a0969dafd646d3dbc9d0
Alexandra <kobatoha@gmail.com>
2023-11-02 20:41:12 +0300

    add bigwar pagan imports

--- Ruoff/bigwars.py
+++ Ruoff/bigwars.py
@@ -6,3 +6,6 @@ from Ruoff.BigWars.bigwar_toi import about_bigwar_toi, set_bigwar_toi, remove_bi
 # [GARDENS]
 from Ruoff.BigWars.bigwar_gardens import about_bigwar_gardens, set_bigwar_gardens, remove_bigwar_gardens, \
     bigwar_gardens_notification_wrapper
+# [PAGAN]
+from Ruoff.BigWars.bigwar_pagan import about_bigwar_pagan, set_bigwar_pagan, remove_bigwar_pagan, \
+    bigwar_pagan_notification_wrapper
