19f2484432521c675a705ff673ed5b7bdf54740f
Alexandra <kobatoha@gmail.com>
2024-01-10 23:28:15 +0300

    fix freetime model

--- DataBase/Freetime.py
+++ DataBase/Freetime.py
@@ -9,18 +9,31 @@ class Freetime(Base):
     id = Column(Integer, primary_key=True)
     id_user = Column(BigInteger, ForeignKey('users.telegram_id'))
 
-    kuka = Column(VARCHAR(5))                   # Кука и Джисра
-    loa_day = Column(VARCHAR(15))               # Логово Антараса [DAY]
+    kuka_time = Column(VARCHAR(5))              # Кука и Джисра [TIME EVERYDAY]
+
+    loa_day_one = Column(VARCHAR(15))           # Логово Антараса [DAY ONE]
+    loa_day_two = Column(VARCHAR(15))           # Логово Антараса [DAY TWO]
     loa_time = Column(VARCHAR(5))               # Логово Антараса [TIME]
-    frost_day = Column(VARCHAR(15))              # Замок Монарха Льда [DAY]
+
+    frost_day_one = Column(VARCHAR(15))         # Замок Монарха Льда [DAY ONE]
+    frost_day_two = Column(VARCHAR(15))         # Замок Монарха Льда [DAY TWO]
     frost_time = Column(VARCHAR(5))             # Замок Монарха Льда [TIME]
-    fortress = Column(VARCHAR(5))               # Крепость Орков
-    balok = Column(VARCHAR(5))                  # Битва с Валлоком
-    olympiad = Column(VARCHAR(5))               # Всемирная Олимпиада
-    hellbound = Column(VARCHAR(5))              # Остров Ада
-    siege = Column(VARCHAR(5))                  # Осада Гирана
-    primetime = Column(VARCHAR(5))              # Хот-тайм зачистки
-    purge = Column(VARCHAR(5))                  # Сбор зачистки
+
+    fortress_time = Column(VARCHAR(5))          # Крепость Орков [TIME EVERYDAY]
+
+    # balok = Column(VARCHAR(15))                    # Битва с Валлоком
+    # olympiad = Column(VARCHAR(5))               # Всемирная Олимпиада
+
+    hellbound_day = Column(VARCHAR(5))          # Остров Ада [DAY]
+    hellbound_time = Column(VARCHAR(5))         # Остров Ада [TIME]
+
+    siege_day = Column(VARCHAR(15))             # Осада Гирана [DAY]
+    siege_time = Column(VARCHAR(5))             # Осада Гирана [TIME]
+
+    # primetime_time = Column(VARCHAR(5))         # Хот-тайм зачистки [EVERYDAY]
+
+    purge_day = Column(VARCHAR(5))              # Сбор зачистки [DAY]
+    purge_time = Column(VARCHAR(5))             # Сбор зачистки [TIME]
 
     event = Column(VARCHAR(5))                  # Ивент [меняется]
     calendar = Column(VARCHAR(5))               # Календарь [меняется]
