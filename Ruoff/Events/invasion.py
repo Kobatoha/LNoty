e56425c1af675003843657796ca9a6d07e035282
Alexandra <kobatoha@gmail.com>
2024-01-17 17:18:10 +0300

    fix invasion

--- Ruoff/Events/invasion.py
+++ Ruoff/Events/invasion.py
@@ -1,6 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher, executor, types, filters
-from datetime import datetime
+from datetime import datetime, timedelta
 from DataBase.User import User
 from DataBase.Base import Base
 from DataBase.Ruoff import Setting
@@ -19,29 +19,45 @@ Session = sessionmaker(bind=engine)
 
 Base.metadata.create_all(engine)
 
-# keber buttons
-inline_keber_buttons = types.InlineKeyboardMarkup()
+# invasion buttons
+inline_invasion_buttons = types.InlineKeyboardMarkup()
 
-set_button = types.InlineKeyboardButton(text='Установить оповещение', callback_data='ruoff_set_keber')
-remove_button = types.InlineKeyboardButton(text='Убрать оповещение', callback_data='ruoff_remove_keber')
+set_button = types.InlineKeyboardButton(text='Установить оповещение', callback_data='ruoff_set_invasion')
+remove_button = types.InlineKeyboardButton(text='Убрать оповещение', callback_data='ruoff_remove_invasion')
 
-inline_keber_buttons.add(set_button, remove_button)
-
-
-# KEBER SETTINGS
-@dp.message_handler(commands=['keber'])
-async def about_keber(message: types.Message):
-    await message.answer('Кебер и его шайка появляется каждый час в :00 минут\n'
-                         'Кебер - само воплощение экспули для твинков\n', reply_markup=inline_keber_buttons)
+inline_invasion_buttons.add(set_button, remove_button)
 
 
-@dp.callback_query_handler(filters.Text(contains='ruoff_set_keber'))
-async def set_keber(callback_query: types.CallbackQuery):
+# INVASION SETTINGS
+@dp.message_handler(commands=['invasion'])
+async def about_invasion(message: types.Message):
+    await message.answer('Вторжение мобов каждые 4 часа в одной из следующих локаций:\n'
+                         '- Долина Драконов (восток)\n'
+                         '- Лагерь Тай\n'
+                         '- Земли Ящеров Мелат\n'
+                         '\n'
+                         'Переместиться к месту вторжения можно от NPC Вранов Коготь,'
+                         ' который появляется перед храмом в Адене, Гиране и Орене.\n'
+                         'Суть мероприятия - колупать мобов и пытаться сделать ластхит промежуточным боссам.'
+                         ' Главный босс появляется через 25 минут после начала вторжения на 10 минут, а затем исчезает'
+                         ' независимо от того, побежден он или нет. За последний удар можно получить сундук'
+                         ' с приятной мелочью:\n'
+                         '- свитки А ранга\n'
+                         '- камни эволюции\n'
+                         '- камни зачарования\n'
+                         '- маг.таблички\n'
+                         '- свитки благословления\n'
+                         '- усилитель краски'
+                         '\n', reply_markup=inline_invasion_buttons)
+
+
+@dp.callback_query_handler(filters.Text(contains='ruoff_set_invasion'))
+async def set_invasion(callback_query: types.CallbackQuery):
     session = Session()
 
     user = session.query(User).filter_by(telegram_id=callback_query.from_user.id).first()
     setting = session.query(Setting).filter_by(id_user=user.telegram_id).first()
-    setting.keber = True
+    setting.invasion = True
 
     session.commit()
 
@@ -50,17 +66,17 @@ async def set_keber(callback_query: types.CallbackQuery):
 
     session.close()
 
-    await callback_query.message.answer('Оповещение для Кебера установлено')
+    await callback_query.message.answer('Оповещение для Вторжения установлено')
     await callback_query.answer()
 
 
-@dp.callback_query_handler(filters.Text(contains='ruoff_remove_keber'))
-async def remove_keber(callback_query: types.CallbackQuery):
+@dp.callback_query_handler(filters.Text(contains='ruoff_remove_invasion'))
+async def remove_invasion(callback_query: types.CallbackQuery):
     session = Session()
 
     user = session.query(User).filter_by(telegram_id=callback_query.from_user.id).first()
     setting = session.query(Setting).filter_by(id_user=user.telegram_id).first()
-    setting.keber = False
+    setting.invasion = False
 
     session.commit()
 
@@ -69,44 +85,55 @@ async def remove_keber(callback_query: types.CallbackQuery):
 
     session.close()
 
-    await callback_query.message.answer('Оповещение для Кебера убрано')
+    await callback_query.message.answer('Оповещение для Вторжения убрано')
     await callback_query.answer()
 
 
-async def keber_notification_wrapper():
+async def invasion_notification_wrapper():
     session = Session()
     users = session.query(User).all()
     for user in users:
 
         setting = session.query(Setting).filter_by(id_user=user.telegram_id).first()
-        if setting.keber is True and setting.fulltime is True:
-            await keber_notification(user)
-        elif setting.keber is True and setting.fulltime is False:
-            await keber_notification_hardwork(user)
+        if setting.invasion is True and setting.fulltime is True:
+            await invasion_notification(user)
+        elif setting.invasion is True and setting.fulltime is False:
+            await invasion_notification_hardwork(user)
     session.close()
 
 
-async def keber_notification(user: User):
+async def invasion_notification(user: User):
     now = datetime.now().strftime('%H:%M')
-    keber_time = ['00:58', '01:58', '02:58', '03:58', '04:58', '05:58', '06:58', '07:58', '08:58', '09:58', '10:58',
-                  '11:58', '12:58', '13:58', '14:58', '15:58', '16:58', '17:58', '18:58', '19:58', '20:58', '21:58',
-                  '22:58', '23:58']
+    invasion_time = ['00:58']
+    invasion = []
+
+    for time in invasion_time:
+        current_time = datetime.strptime(time, "%H:%M")
+        for i in range(6):  # Добавляем 4 часа 6 раз (через каждые 4 часа)
+            invasion.append(current_time.strftime("%H:%M"))
+            current_time += timedelta(hours=4)
+
     try:
-        if now in keber_time:
-            await mybot.send_message(user.telegram_id, 'Кебер появится через 2 минуты')
-            print(now, user.telegram_id, user.username, '(круглосуточник) получил сообщение о Кебере')
+        if now in invasion:
+            await mybot.send_message(user.telegram_id, 'Вторжение начнется через 5 минут. Следите за анонсом в игре.')
+            print(now, user.telegram_id, user.username, '(круглосуточник) получил сообщение о Вторжении')
     except BotBlocked:
         print('[ERROR] Пользователь заблокировал бота:', now, user.telegram_id, user.username)
 
 
-async def keber_notification_hardwork(user: User):
+async def invasion_notification_hardwork(user: User):
     now = datetime.now().strftime('%H:%M')
-    keber_hardwork = ['08:58', '09:58', '10:58', '11:58', '12:58', '13:58', '14:58', '15:58', '16:58', '17:58', '18:58',
-                      '19:58', '20:58', '21:58', '22:58']
+    invasion_time = ['00:58']
+    invasion_hardwork = []
 
+    for time in invasion_time:
+        current_time = datetime.strptime(time, "%H:%M")
+        for i in range(6):  # Добавляем 4 часа 6 раз (через каждые 4 часа)
+            invasion_hardwork.append(current_time.strftime("%H:%M"))
+            current_time += timedelta(hours=4)
     try:
-        if now in keber_hardwork:
-            await mybot.send_message(user.telegram_id, 'Кебер появится через 2 минуты')
-            print(now, user.telegram_id, user.username, '(работяга) получил сообщение о Кебере')
+        if now in invasion_hardwork:
+            await mybot.send_message(user.telegram_id, 'Вторжение начнется через 5 минут. Следите за анонсом в игре.')
+            print(now, user.telegram_id, user.username, '(работяга) получил сообщение о Вторжении')
     except BotBlocked:
         print('[ERROR] Пользователь заблокировал бота:', now, user.telegram_id, user.username)
