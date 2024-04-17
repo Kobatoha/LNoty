50a7a6a4a966c8f793d5d0921eafbce28d349446
Kanian Tai <166604509+EitheryMJ@users.noreply.github.com>
2024-04-17 17:59:15 +0300

    Create lore.py

---
+++ lore.py
@@ -0,0 +1,40 @@
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN, DB_URL
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from DataBase.Base import Base
+from DataBase.User import User
+from DataBase.Ruoff import Setting, RuoffCustomSetting, RuoffClanDangeon
+from DataBase.RaidBoss import RaidBoss
+from DataBase.Feedback import Feedback
+from aiocron import crontab
+import asyncio
+from datetime import datetime
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+mybot = Bot(token=TOKEN)
+dp = Dispatcher(mybot, storage=MemoryStorage())
+
+engine = create_engine(DB_URL)
+
+Session = sessionmaker(bind=engine)
+
+Base.metadata.create_all(engine)
+
+lore_path = [
+  """
+  Баюм - императором Эльморадена возвел Башню для обретения вечной жизни. 
+  Дерзость Баюма навлекла на него гнев Эйнхасад. Рассказывают, что Баюм в итоге 
+  обрел вечную жизнь и вечную молодость, как сам того хотел, но взамен лишился свободы. 
+  Он стал пленником собственной башни и сошел там с ума.
+  """,
+  """
+  
+  """
+]
+
+
+@dp.message_handler()
+async def echo_lore(message: types.Message):
+    await message.answer(lore_part)
