1bfd1af3f0f324abbdd20c13b8bc2c5bf2eb7e6a
Alexandra <kobatoha@gmail.com>
2024-05-31 23:49:19 +0300

    update and fix server legacy button

--- Ruoff/Events/invasion.py
+++ Ruoff/Events/invasion.py
@@ -104,7 +104,7 @@ async def invasion_notification_wrapper():
 
 async def invasion_notification(user: User):
     now = datetime.now().strftime('%H:%M')
-    invasion_time = ['20:22']
+    invasion_time = ['10:52']
     invasion = []
 
     for time in invasion_time:
@@ -123,7 +123,7 @@ async def invasion_notification(user: User):
 
 async def invasion_notification_hardwork(user: User):
     now = datetime.now().strftime('%H:%M')
-    invasion_time = ['20:22']
+    invasion_time = ['10:52']
     invasion_hardwork = []
 
     for time in invasion_time:
